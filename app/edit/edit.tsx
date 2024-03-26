77b60285ab2705ce6467af75fc60f01f7aaab2b4
Loyd Cose <103356720+loydcose@users.noreply.github.com>
2024-03-26 15:02:34 +0800

    WIP: edit profile submission

--- app/edit/edit.tsx
+++ app/edit/edit.tsx
@@ -16,38 +16,23 @@ type TEdit = {
 }
 
 export default function Edit({user, socials}: TEdit) {
-    const {id, imageUrl, name, bio, isActivated} = user
-    const [formObj, setFormObj] = useState({
-        name: user.name,
-        username: user.username,
-        imageUrl: user.imageUrl,
-        bio: user.bio,
-        isActivated: user.isActivated,
-        socials: socials.map(social => ({
-            name: social.name,
-            slug: social.slug,
-            link: social.link,
-            socialMedia: social.socialMedia
-        }))
-    }) 
-
-    console.log({socials})
-
+    const {id, imageUrl, name, bio, isActivated, username} = user
 
     const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        console.log(Array.from(formData.entries()));
+        let formData: any = new FormData(e.currentTarget);
+        formData = Object.fromEntries(formData.entries());
     }
 
   return (
     <form onSubmit={handleFormSubmit}>  
         <div className='flex flex-col gap-4 py-10 border-b border-b-stone-300'>
-            <ImageUpload formObj={formObj} setFormObj={setFormObj}/>
+            {/* <ImageUpload formObj={formObj} setFormObj={setFormObj}/> */}
             <Input placeholder='Enter your name' name='name' defaultValue={name}/>
+            <Input placeholder='Enter your username' name='username' defaultValue={username}/>
             <Input placeholder='Enter your bio' name='bio' defaultValue={bio || ""}/>
         </div>
-        <EditSocials formObj={formObj} setFormObj={setFormObj}/>
+        <EditSocials socials={socials}/>
         <div className='py-10 flex gap-2 w-fit ml-auto'>
             <Link href="/" className={buttonVariants({ variant: "outline" })}>Cancel</Link>
             <Button >Save</Button>
--- components/edit-socials.tsx
+++ components/edit-socials.tsx
@@ -3,27 +3,41 @@
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Social } from '@prisma/client'
+import { useState } from 'react'
 
 type TEditSocials = {
     socials: Social[]
 }
 
 export default function EditSocials({socials}: TEditSocials) {
+    const [socialFields, setSocialFields] = useState(socials)
     
+    // GOLO MAG ISIP KA KAYA MUNA PRE..
+    const addSocialField = () => {
+        // @ts-ignore
+        setSocialFields(prev => ([...prev, {
+            id: new Date().getTime().toString(),
+            socialMedia: "",
+            name: "",
+            link: ""
+        }]))
+    }
+
+    console.log({socialFields})
 
 
   return (
     <div className='space-y-3 py-10 border-b border-b-stone-300'>
-        {socials.map(social => (
-            <div className='mb-8'>
+        {socialFields && socialFields.map((social, index) => (
+            <div className='mb-8' key={social.id}>
                 <div className='grid grid-cols-2 gap-2 mb-2'>
-                    <Input placeholder='Enter social media' defaultValue={social.socialMedia}/>
-                    <Input placeholder='Enter display name' defaultValue={social.name}/>
+                    <Input placeholder='Enter social media' value={social.socialMedia} onChange={e => socials[index].socialMedia = e.target.value}/>
+                    <Input placeholder='Enter display name' value={social.name} onChange={e => socials[index].name = e.target.value}/>
                 </div>
-                <Input placeholder='Enter your link' defaultValue={social.link}/>
+                <Input placeholder='Enter your link' value={social.link} onChange={e => socials[index].link = e.target.value}/>
             </div>
         ))}
-        <Button className='mt-6 w-full' variant={"secondary"} size={"sm"}>Add new</Button>
+        <Button className='mt-6 w-full' onClick={addSocialField} variant={"secondary"} size={"sm"}>Add new</Button>
     </div>
 )
 }
