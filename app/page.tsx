598fa9731bdc628730ddfbaed9f385bcd2281d92
Loyd Cose <103356720+loydcose@users.noreply.github.com>
2024-04-16 14:40:24 +0800

    Add placeholder for empty social media list

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import ProfileImage from "@/components/profile-image";
 import ProfileInfo from "@/components/profile-info";
 import PublicBadge from "@/components/public-badge";
 import SocialLinks from "@/components/social-links";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 
 export default async function Home() {
@@ -18,12 +21,26 @@ export default async function Home() {
     }
   }
 
+  console.log({ socials });
+
   return (
     <>
       <PublicBadge user={user} />
       <ProfileImage userImage={user.imageUrl} />
       <ProfileInfo user={user} />
-      <SocialLinks socials={socials || []} />
+      {!socials || socials.length === 0 ? (
+        <div className="flex flex-col gap-2 text-center border border-gray-200 rounded-xl p-4 mx-auto md:w-[75%]">
+          <p>No social media found.</p>
+          <Link
+            href="/settings"
+            className={cn(buttonVariants({ variant: "default", size: "sm" }))}
+          >
+            Start creating one
+          </Link>
+        </div>
+      ) : (
+        <SocialLinks socials={socials || []} />
+      )}
     </>
   );
 }
--- components/profile-info.tsx
+++ components/profile-info.tsx
@@ -5,7 +5,7 @@ export default function ProfileInfo({ user }: { user: User }) {
     <>
       <h1 className="text-center font-bold text-xl md:text-2xl">{user.name}</h1>
       <p className="text-sm text-gray-600 text-center mb-2">{user.username}</p>
-      <p className="mb-8 text-center text-stone-600">{user.bio}</p>
+      <p className="mb-12 text-center text-stone-600">{user.bio}</p>
     </>
   );
 }
