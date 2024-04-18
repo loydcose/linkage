c3de287d761e0ba5e43688afc0c0715fa9b193c3
Loyd Cose <103356720+loydcose@users.noreply.github.com>
2024-04-18 17:47:21 +0800

    Fix bug on the /:username

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,33 +1,34 @@
-import { getUserByUsername } from "@/actions"
-import ProfileImage from "@/components/profile-image"
-import ProfileInfo from "@/components/profile-info"
-import SocialLinks from "@/components/social-links"
-import { ArrowUpRight } from "lucide-react"
-import Link from "next/link"
-import { notFound } from "next/navigation"
+import { getUserByUsername, getUserSocials } from "@/actions";
+import ProfileImage from "@/components/profile-image";
+import ProfileInfo from "@/components/profile-info";
+import SocialLinks from "@/components/social-links";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export default async function Home({
   params: { username },
 }: {
-  params: { username: string }
+  params: { username: string };
 }) {
-  const user = await getUserByUsername(username)
+  const user = await getUserByUsername(username);
+  const socials = await getUserSocials();
 
   if (!user) {
-    notFound()
+    notFound();
   }
 
   return (
     <div className="h-full flex flex-col">
       <ProfileImage userImage={user.imageUrl} />
       <ProfileInfo user={user} />
-      <SocialLinks socials={user.socials || []} />
+      <SocialLinks socials={socials || []} />
       <Link
         href="/"
         className="text-gray-600 text-sm justify-center mt-auto hover:text-purple-600 transition-colors flex items-center gap-2"
       >
-        Create my own linkage profile <ArrowUpRight size={16}/>
+        Create my own linkage profile <ArrowUpRight size={16} />
       </Link>
     </div>
-  )
+  );
 }
