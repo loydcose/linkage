da99e294aa920f40609fdc520c922bffaa1cda9e
Loyd Cose <103356720+loydcose@users.noreply.github.com>
2024-04-22 17:28:02 +0800

    Fix navbar style

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -2,21 +2,40 @@
 
 import { UserButton } from "@clerk/nextjs";
 
+import { cn } from "@/lib/utils";
+import { Settings } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import TopRightBtn from "./top-right-btn";
+import { buttonVariants } from "./ui/button";
 
 export default function TopNav() {
   const pathname = usePathname();
   const isNotInPublicView = pathname === "/edit" || pathname === "/";
 
   return (
-    <nav className="flex justify-between items-center mb-8 w-[90%] mx-auto max-w-xl py-8">
-      {isNotInPublicView && <UserButton />}
-      <Link href="/" className="hover:text-purple-600 transition-colors w-full">
-        <h1 className="font-bold text-xl text-center">Linkage</h1>
+    <nav className="relative mb-8 w-[90%] mx-auto max-w-xl py-8">
+      {isNotInPublicView && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2">
+          <UserButton />
+        </div>
+      )}
+      <Link
+        href="/"
+        className="w-fit block absolute hover:text-purple-600 transition-colors top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
+      >
+        <h1 className="font-bold text-xl">Linkage</h1>
       </Link>
-      {isNotInPublicView && <TopRightBtn />}
+      {isNotInPublicView && pathname === "/" && (
+        <Link
+          href="/edit"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "w-8 h-8 p-0 flex absolute right-0 top-1/2 -translate-y-1/2"
+          )}
+        >
+          <Settings className="m-auto text-xs text-stone-500" />
+        </Link>
+      )}
     </nav>
   );
 }
